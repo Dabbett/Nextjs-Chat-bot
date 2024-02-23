166c11d8f9d64f59be61a7d08d158c800606ff01
Dabbett <abbettdylan@gmail.com>
2024-02-23 11:03:29 -0500

    inital working chat commit

---
+++ app/api/route.ts
@@ -0,0 +1,34 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Configuration, OpenAIApi } from "openai-edge";
+import { prisma } from "../db";
+import { log } from "console";
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_AI_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+// export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages: messages,
+  });
+
+  const stream = OpenAIStream(response, {
+    onCompletion: async (completion: string) => {
+      const data = await prisma.message.create({
+        data: {
+          answer: completion,
+          question: messages.slice(-1)[0].content,
+        },
+      })
+    },
+  });
+  return new StreamingTextResponse(stream);
+}
